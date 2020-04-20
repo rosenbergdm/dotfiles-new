095bd1a908a270243c1a30e6d1d7b37e8ef3a0b9
David Rosenberg <david.m.rosenberg@gmail.com>
2020-04-20 16:30:14 -0500

    Fix typo

--- bin/dotfiles-check.sh
+++ bin/dotfiles-check.sh
@@ -15,13 +15,14 @@ DOTFILES_LOGFILE="/var/log/dotfiles-check.log"
 declare -a modified_files
 modified_files=( $($GIT_EXE --git-dir="$DOTFILES_GIT_DIR/" --work-tree="$WORK_DIR" status | grep modified | awk '{print $2}' ) )
 modified_status=$?
-if [ $modified_status -eq 0 ]; then
+if [ "${modified_files[@]}:" = ":" ]; then
+  echo "All dotfiles managed files committed on $(date)" | tee $DOTFILES_LOGFILE
+else
   for f in ${modified_files[@]}; do
     echo "Dotfiles managed file $f is modified but not committed on $(date)" | tee $DOTFILES_LOGFILE
   done
   exit 1
-else
-  echo "All dotfiles managed files committed on $(date)" | tee $DOTFILES_LOGFILE
 fi
+
 exit 0
 
