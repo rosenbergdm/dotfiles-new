248bc7c51c919d0575ee7165a9f6e38c91809452
David Rosenberg <dmr@davidrosenberg.me>
2020-05-07 22:12:59 -0500

    Updated to rsync Box directory nightly

--- crontab.sh
+++ crontab.sh
@@ -4,6 +4,8 @@ MAILTO=davidrosenberg
 @daily  /Users/davidrosenberg/bin/gmvault-runner.sh
 @monthly  /Users/davidrosenberg/bin/backup_my_passwords.sh 2>&1 >> /var/log/backup_my_passwords.log
 @daily  /Users/davidrosenberg/bin/dotfiles-check.sh
+@daily  /usr/bin/rsync -ahp /Users/davidrosenberg/Box/ /Volumes/LaptopBackup2/uic-backups/Box
+
 
 
 
