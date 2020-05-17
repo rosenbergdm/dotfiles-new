47dce0ff4b24ce920c6c0aa712e5e240e8000a0d
David Rosenberg <dmr@davidrosenberg.me>
2020-05-17 14:35:00 -0500

    SUpport for styling R in vim

--- lib/R/lintr.R
+++ lib/R/lintr.R
@@ -1,7 +1,8 @@
 #! /usr/bin/env R
 library(lintr)
 my_linter <- with_defaults(line_length_linter(120),
-                           commented_code_linter = NULL)
+  commented_code_linter = NULL
+)
 target_file <- commandArgs(trailingOnly = TRUE)
 lint(target_file, linters = my_linter)
 
