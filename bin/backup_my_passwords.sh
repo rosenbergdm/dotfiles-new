9ac356d7684e85bc5d2827f0339f80f8486dcda8
David Rosenberg <dmr@davidrosenberg.me>
2020-08-06 18:02:18 -0400

    update password filename

--- bin/backup_my_passwords.sh
+++ bin/backup_my_passwords.sh
@@ -2,11 +2,19 @@
 source "$HOME/.functions"
 
 cd $HOME/Dropbox
-tgtfile="$HOME/Dropbox/passwords-backup/passwords-$(date +%Y%m%d).kdbx"
+tgtfile="$HOME/Dropbox/passwords-backup/passwords-v2-$(date +%Y%m%d).kdbx"
+keyfile="$HOME/Dropbox/passwords-backup/keepass-v2-$(date +%Y%m%d).key"
 if [[ -f "$tgtfile"  ]]; then
     echo "FILE ALREADY EXISTS"
 else
-    cp "./passwords.kdbx" "$tgtfile"
+    cp "./passwords-v2.kdbx" "$tgtfile"
     echo "FILE COPY of passwords.kdbx to $tgtfile COMPLETE"
 fi
+if [[ -f "$keyfile" ]]; then
+  echo "FILE ALREADY EXISTS"
+else
+  cp "./keepass2" "$keyfile"
+  echo "FILE COPY of keepass2 to $keyfile COMPLETE"
+fi
+
 
