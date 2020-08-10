22dd4d92a364c61f9d674c3f1c42231a817fe88e
David Rosenberg <dmr@davidrosenberg.me>
2020-08-09 20:46:43 -0400

    added script to start virtual console via idrac

---
+++ bin/recreate_completions.sh
@@ -0,0 +1,62 @@
+#!/usr/local/bin/bash
+#
+# recreate_completions.sh
+# Copyright (C) 2020 David Rosenberg <dmr@davidrosenberg.me>
+#
+# Create bash completion scripts instead of having them generated from their
+# respective commands at login.  This should be run regularly from 
+# cron.
+#
+# Distributed under terms of the MIT license.
+#
+
+COMPLETION_DIR="$HOME/bash_completion.d"
+LOGFILE="/var/log/recreate_completions.log"
+tmpfile="$(mktemp -t rcrt.compl)"
+script_status=0
+
+cleanup() {
+  rm -f "$tmpfile"
+}
+trap cleanup EXIT
+
+# General format:
+# CMD > "$tmpfile" 2>/dev/null && \
+#   mv "$tmpfile" "$COMPLETION_DIR/CMD"  && \
+#   echo "$(date): Recreated CMD completion script" | tee -a "$LOGFILE" || \
+#   echo "$(date): Unable to regenerate CMD completion script" | tee -a "$LOGFILE" > /dev/stderr && \
+#   false
+# script_status=$((script_status+$?))
+
+#stack
+
+/usr/local/bin/stack --bash-completion-script stack > "$tmpfile" 2>/dev/null && \
+  mv "$tmpfile" "$COMPLETION_DIR/stack"  && \
+  echo "$(date): Recreated stack completion script" | tee -a "$LOGFILE" || \
+  echo "$(date): Unable to regenerate stack completion script" | tee -a "$LOGFILE" > /dev/stderr && \
+  false
+script_status=$((script_status+$?))
+
+#pandoc
+
+/Users/davidrosenberg/.cabal/bin/pandoc --bash-completion > "$tmpfile" 2>/dev/null && \
+  mv "$tmpfile" "$COMPLETION_DIR/pandoc"  && \
+  echo "$(date): Recreated pandoc completion script" | tee -a "$LOGFILE" || \
+  echo "$(date): Unable to regenerate pandoc completion script" | tee -a "$LOGFILE" > /dev/stderr && \
+  false
+script_status=$((script_status+$?))
+
+#pandoc
+/usr/local/bin/pip completion --bash > "$tmpfile" 2>/dev/null && \
+  mv "$tmpfile" "$COMPLETION_DIR/pip"  && \
+  echo "$(date): Recreated pip completion script" | tee -a "$LOGFILE" || \
+  echo "$(date): Unable to regenerate pip completion script" | tee -a "$LOGFILE" > /dev/stderr && \
+  false
+script_status=$((script_status+$?))
+
+
+trap - EXIT
+rm -f "$tmpfile" 
+exit $script_status
+
+# vim: ft=sh
