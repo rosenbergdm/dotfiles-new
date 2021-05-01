15c8c96ea7acb10bbf19e3e2c97f45a31932c0dc
David Rosenberg <dmr@davidrosenberg.me>
2021-04-30 22:12:57 -0400

    use brew fzf

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */Users/davidrosenberg/src/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/Users/davidrosenberg/src/fzf/bin"
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/Users/davidrosenberg/src/fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/Users/davidrosenberg/src/fzf/shell/key-bindings.bash"
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
